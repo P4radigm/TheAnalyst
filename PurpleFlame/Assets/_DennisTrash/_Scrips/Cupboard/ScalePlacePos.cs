097b558778c55386716c45ba713b0b7d85ba410e
DennisBorst <Dennis.Borst@hotmail.nl>
2021-01-07 16:49:11 +0000

    Cupboard puzzle finished

---
+++ PurpleFlame/Assets/_DennisTrash/_Scrips/Cupboard/ScalePlacePos.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dennis 
+{
+    public class ScalePlacePos : MonoBehaviour
+    {
+        public float disToWeightPos;
+        [HideInInspector] public Weight placeOccupied;
+
+        [SerializeField] private int side; //0=left 1=right;
+        [Space]
+        [SerializeField] private Scale scaleScript;
+        [SerializeField] private Transform weightPos;
+
+        private int currentPlaceOccupied;
+
+        public void AddWeight(Weight currentWeight)
+        {
+            placeOccupied = currentWeight;
+            placeOccupied.transform.position = weightPos.position;
+            placeOccupied.transform.parent = weightPos;
+            scaleScript.AddWeightToScale(placeOccupied, side);
+        }
+
+        public void RemoveWeight()
+        {
+            scaleScript.RemoveWeight(side);
+            placeOccupied.SetParentToMain();
+            placeOccupied = null;
+        }
+    }
+}
---
+++ PurpleFlame/Assets/_DennisTrash/_Scrips/Cupboard/Weight.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dennis
+{
+    public class Weight : MonoBehaviour
+    {
+        public int heavyWeight;
+        public WeightPlacePos lastWeightPos;
+        [HideInInspector] public Vector3 lastPos;
+
+        private Collider collider;
+        private WeightPlacePos weightPlacePos;
+        private ScalePlacePos scalePlacePos;
+        private Transform mainParent;
+
+        private void Start()
+        {
+            lastWeightPos.AddWeight(this);
+            ResetPosition();
+            collider = GetComponent<Collider>();
+            mainParent = this.transform.parent;
+        }
+
+        public void DisableCollider(bool state)
+        {
+            collider.enabled = !state;
+        }
+
+        public void ResetPosition()
+        {
+            this.transform.position = lastPos;
+            if(lastWeightPos.heavyness != 0) { return; }
+            lastWeightPos.placeOccupied = this;
+        }
+
+        public void UpdatePosition(Vector3 hitPos)
+        {
+            this.transform.position = hitPos;
+        }
+
+        public void CheckCurrentWeightPos()
+        {
+            if(lastWeightPos != null)
+            {
+                lastWeightPos.RemoveWeight();
+            }
+
+            if(weightPlacePos != null) 
+            {
+                weightPlacePos.RemoveWeight();
+                weightPlacePos = null;
+            }
+            if(scalePlacePos != null)
+            {
+                scalePlacePos.RemoveWeight();
+                scalePlacePos = null;
+            }
+        }
+
+        public void AddCurrentWeightPosition(WeightPlacePos currentPos)
+        {
+            weightPlacePos = currentPos;
+            lastWeightPos = currentPos;
+        }
+
+        public void AddCurrentWeightPosScale(ScalePlacePos currentPos)
+        {
+            scalePlacePos = currentPos;
+        }
+
+        public void SetParentToMain()
+        {
+            this.transform.parent = mainParent;
+        }
+
+        public void SetStartPos(Vector3 newPos)
+        {
+            lastPos = newPos;
+        }
+    }
+}
