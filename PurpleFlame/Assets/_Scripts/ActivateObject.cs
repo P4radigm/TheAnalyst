fb423aebabfc2235ee44ffdfd0049f9308797e51
Thijs Roem <thijsroem@gmail.com>
2021-01-13 14:33:57 +0100

    ACT1 cutscene update

---
+++ PurpleFlame/Assets/_Scripts/ActivateObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateObject : MonoBehaviour
+{
+    [SerializeField] private GameObject[] objectToActivate;
+
+    void ActivateObjectTrue()
+    {
+        for (int i = 0; i < objectToActivate.Length; i++)
+        {
+            objectToActivate[i].SetActive(true);
+        }
+        
+    }
+}
